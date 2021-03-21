62161e2f317d99bbc2b4c94fe97d07c1819814e3
chloes0mt <78044999+chloes0mt@users.noreply.github.com>
2021-03-20 22:31:31 -0400

    Update in.js

--- in.js
+++ in.js
@@ -1,6 +1,6 @@
 alert("WARNING: Your device has been infected with a virus. Please contact tech support immediately.");
 
-var audio = new Audio('file:///Users/chloetremmel/Music/iTunes/iTunes%20Media/Music/Unknown%20Artist/Unknown%20Album/Nebula%20-%20myuu.mp3');
+var audio = new Audio('Nebula - myuu.mp3');
 
 function go() {
     document.body.style.backgroundColor = "red";
